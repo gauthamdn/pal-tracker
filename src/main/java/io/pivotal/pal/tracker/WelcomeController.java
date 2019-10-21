88230745f7301570ca8fcf6d04dd09ff05041d8b
SyamalaDora <syamala.dora@emc.com>
2019-10-21 16:57:36 +0530

    Lab 2 env variables code

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,24 @@
 package io.pivotal.pal.tracker;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String WelcomeMsg = "";
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return WelcomeMsg;
     }
+
+
+    public  WelcomeController(@Value("${Welcome.message}") String message){
+        WelcomeMsg =  message;
+
+    }
+
 }
